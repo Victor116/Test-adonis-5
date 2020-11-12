cc223e00eb69283eedae993e82c76ede5bfaeedf
victor116 <victor19961996@live.com.mx>
2020-11-12 13:44:12 -0600

    Correccion ruta wheels y controller

--- start/routes/wheel.ts
+++ start/routes/wheel.ts
@@ -2,14 +2,14 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     
-  Route.get('/', 'WheelController.index')
+  Route.get('/', 'WheelsController.index')
   
-  Route.post('/', 'WheelController.create')
+  Route.post('/', 'WheelsController.create')
   
-  Route.get('/:id', 'WheelController.show')
+  Route.get('/:id', 'WheelsController.show')
   
-  Route.put('/:id', 'WheelController.update')
+  Route.put('/:id', 'WheelsController.update')
   
-  Route.delete('/:id', 'WheelController.delete')
+  Route.delete('/:id', 'WheelsController.destroy')
   
-}).prefix('/api/v1/wheel').where('id', /^[0-9]+$/)
+}).prefix('/api/v1/wheels').where('id', /^[0-9]+$/)
